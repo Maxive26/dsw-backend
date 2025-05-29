6a78741797976786f0fe6c2634ead3dd6aa7386a
cirogiam58 <cirogiam58@gmail.com>
2025-05-29 13:10:13 -0300

    Revert "Eliminar la clase Sucursal y reemplazarla con una función para inicializar la tabla sucursal en la base de datos"
    This reverts commit 84e6661eff7823181cabe0c8595198777b4a8e9c.

--- src/models/sucursal.js
+++ src/models/sucursal.js
@@ -1,11 +1,9 @@
-import { db } from "../config/db.js";
-
-export default async function initSucursalTable() {
-  await db.execute(`
-    CREATE TABLE IF NOT EXISTS sucursal (
-      idSucursal INTEGER PRIMARY KEY,
-      nombre TEXT NOT NULL,
-      direccion TEXT NOT NULL
-    );
-  `);
+class Sucursal {
+  constructor(idSucursal, nombre, direccion) {
+    this.idSucursal = idSucursal;
+    this.nombre = nombre;
+    this.direccion = direccion;
+  }
 }
+
+export default Sucursal;
